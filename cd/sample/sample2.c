2bdc340e7042e29c33082833865da93adcfb284e
Prasanthi G <56592188+Prash2002@users.noreply.github.com>
2022-05-05 12:34:23 +0530

    Add files via upload

--- cd/sample/sample2.c
+++ cd/sample/sample2.c
@@ -5,7 +5,6 @@ int main(){
     char a[50] ="Hi, this a normal string";
     int x, y;
     printf("A normal statement");
-    //comment
     char b[100]=" this is a problem 
     fosho; ";
 
---
+++ cd/sample/sample3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    char a[50] ="Hi, this a normal string";
+    int x = 65 , y= 3 ;
+    float z = 24.5;
+    double c = 3..2;
+    double e = 3.a1;
+    int r =3a;
+
+
+    return 0;
+}
